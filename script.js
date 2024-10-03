204c240c8cf6185e07c86cece52f345c38dc1159
Alina Vinogradova <ellafoss@yandex.ru>
2024-10-03 03:54:22 +0300

    Урок 2. Присвоили значения, вывод в консоль

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let title = "", screens, screenPrice = 0, rollback,
-    fullPrice = 0, adaptive;
+'use strict';
 
-alert("Любой текст");
-console.log("Текст в консоли");
+const title = "Проект калькулятор";
+const rollback = 25;
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let fullPrice = 10000;
+let adaptive = false;
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+screens = screens.toLowerCase().split(", ");
+console.log(screens);
+
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)) + " рублей");
