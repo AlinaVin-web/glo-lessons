fc090fd92a8b15c8044e6aacddeff42642eb3239
Alina Vinogradova <ellafoss@yandex.ru>
2024-10-06 03:18:57 +0300

    Правка. Присвоение значений переменным сразу.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 'use strict';
 
-let title = "Проект калькулятор";
 const rollback = 25;
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 500;
-let fullPrice = 10000;
-let adaptive = false;
-let service1, service2;
-let servicePrice1 = 0, servicePrice2 = 0;
-let servicePercentPrice = 0;
+let title = prompt('Как называется ваш проект?', 'Проект калькулятор');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга 1');
+let servicePrice1 = +prompt('Сколько это будет стоить?', '2000');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга 2');
+let servicePrice2 = +prompt('Сколько это будет стоить?', '4500');
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
 console.log(screens.length);
@@ -17,21 +20,6 @@ console.log("Стоимость разработки сайта " + fullPrice + 
 screens = screens.toLowerCase().split(", ");
 console.log(screens);
 console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)) + " рублей");
-
-// Урок 3
-
-title = prompt('Как называется ваш проект?', 'Проект калькулятор');
-screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
-adaptive = confirm("Нужен ли адаптив на сайте?");
-service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга 1');
-servicePrice1 = +prompt('Сколько это будет стоить?', '2000');
-service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга 2');
-servicePrice2 = +prompt('Сколько это будет стоить?', '4500');
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 console.log(servicePercentPrice);
 
 switch (true) {
@@ -51,4 +39,3 @@ switch (true) {
         console.log("Что-то пошло не так");
         break;
 }
-
